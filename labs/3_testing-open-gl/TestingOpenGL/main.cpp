59c9b3cc5d351e84cb167fee3102c7070860577c
Vladimir <fruityflunt@gmail.com>
2024-03-03 09:11:31 +0300

    Testing OpenGL: Setup window. And also added clang-format file

--- labs/3_testing-open-gl/TestingOpenGL/main.cpp
+++ labs/3_testing-open-gl/TestingOpenGL/main.cpp
@@ -2,6 +2,33 @@
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
 
-int main() {
+int main()
+{
+	glfwInit();
+
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+	GLFWwindow* window = glfwCreateWindow(800, 600, "Testing Open GL", NULL, NULL);
+
+	if (window == NULL)
+	{
+		std::cout << "Failed to create GLFW window" << std::endl;
+		glfwTerminate();
+
+		return 1;
+	}
+
+	glfwMakeContextCurrent(window); // Make the window in current context
+
+	while (!glfwWindowShouldClose(window)) // Hold window opened until its closed by user or other function
+	{
+		glfwPollEvents(); // Process all the poll events like a dragging resizing and etc
+	}
+
+	glfwDestroyWindow(window); // Destroying after close
+	glfwTerminate();
+
 	return 0;
 }
