f82da1b938d7f0cd5a7832d5415a6e5e420d5f07
Vladimir <fruityflunt@gmail.com>
2024-03-03 11:05:10 +0300

    Testing OpenGL: Setup buffer's new color and adding comments

--- labs/3_testing-open-gl/TestingOpenGL/main.cpp
+++ labs/3_testing-open-gl/TestingOpenGL/main.cpp
@@ -4,31 +4,52 @@
 
 int main()
 {
+	// Initialize GLFW
 	glfwInit();
 
+	// Tell GLFW what version of OpenGL we are using
+	// In this case we are using OpenGL 3.3
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	// Tell GLFW we are using the CORE profile
+	// So that means we only have the modern functions
 	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
-	GLFWwindow* window = glfwCreateWindow(800, 600, "Testing Open GL", NULL, NULL);
-
+	// Create a GLFWwindow object of 800 by 800 pixels, naming it "YoutubeOpenGL"
+	GLFWwindow* window = glfwCreateWindow(800, 800, "YoutubeOpenGL", NULL, NULL);
+	// Error check if the window fails to create
 	if (window == NULL)
 	{
 		std::cout << "Failed to create GLFW window" << std::endl;
 		glfwTerminate();
-
-		return 1;
+		return -1;
 	}
+	// Introduce the window into the current context
+	glfwMakeContextCurrent(window);
 
-	glfwMakeContextCurrent(window); // Make the window in current context
+	// Load GLAD so it configures OpenGL
+	gladLoadGL();
+	// Specify the viewport of OpenGL in the Window
+	// In this case the viewport goes from x = 0, y = 0, to x = 800, y = 800
+	glViewport(0, 0, 800, 800);
 
-	while (!glfwWindowShouldClose(window)) // Hold window opened until its closed by user or other function
+	// Specify the color of the background
+	glClearColor(0.07f, 0.13f, 0.17f, 1.0f);
+	// Clean the back buffer and assign the new color to it
+	glClear(GL_COLOR_BUFFER_BIT);
+	// Swap the back buffer with the front buffer
+	glfwSwapBuffers(window);
+
+	// Main while loop
+	while (!glfwWindowShouldClose(window))
 	{
-		glfwPollEvents(); // Process all the poll events like a dragging resizing and etc
+		// Take care of all GLFW events
+		glfwPollEvents();
 	}
 
-	glfwDestroyWindow(window); // Destroying after close
+	// Delete window before ending the program
+	glfwDestroyWindow(window);
+	// Terminate GLFW before ending the program
 	glfwTerminate();
-
 	return 0;
 }
