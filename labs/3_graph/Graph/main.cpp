d765ab3edd5f12e20487671803de1172a4abe0d1
Vladimir <fruityflunt@gmail.com>
2024-03-08 15:52:10 +0300

    lab#3: Парабола переделал, написал комменты

--- labs/3_graph/Graph/main.cpp
+++ labs/3_graph/Graph/main.cpp
@@ -1,81 +1,65 @@
-﻿#include <cmath>
+﻿#include <GLFW/glfw3.h>
 #include <iostream>
-#include <vector>
 
-#include <GLFW/glfw3.h>
+void drawCoordinateAxes()
+{
+	glColor3f(1.0, 1.0, 1.0); // установить цвет линий
 
-const int WIDTH = 1000;
-const int HEIGHT = 1000;
-
+	glBegin(GL_LINES);
+	// Ось X
+	glVertex2f(-10.0, 0.0);
+	glVertex2f(10.0, 0.0);
+	// Ось Y
+	glVertex2f(0.0, -10.0);
+	glVertex2f(0.0, 10.0);
+	glEnd();
+}
 
 void drawParabola(float a, float b, float c)
 {
-	glPointSize(1.0f); // Set point size for better visibility
-	glColor3f(1.0f, 1.0f, 1.0f); // Set color to white
+	glColor3f(1.0, 0.0, 0.0); // красный цвет для графика параболы
 
 	glBegin(GL_POINTS);
-	for (float x = -10.0f; x <= 10.0f; x += 0.01f)
+	for (float x = -10.0; x <= 10.0; x += 0.01)
 	{
-		float y = a * x * x + b * x + c;
-		glVertex2f(x, y);
+		float y = a * x * x + b * x + c; // вычислить значение y для заданного x
+		glVertex2f(x, y); // нарисовать точку с координатами (x, y)
 	}
 	glEnd();
 }
 
-void drawX(float cx, float cy)
+void display()
 {
-	glColor3f(1.0f, 1.0f, 1.0f);
-	glBegin(GL_LINES);
-	glVertex2f(cx - 100.0f, cy);
-	glVertex2f(cx + 100.0f, cy);
-	glEnd();
-}
+	glClear(GL_COLOR_BUFFER_BIT);
 
-void resize(int width, int height)
-{
-	glViewport(0, 0, width, height);
-	glMatrixMode(GL_PROJECTION);
-	glLoadIdentity();
+	drawCoordinateAxes(); // нарисовать оси координат
+	drawParabola(10, 0, 0); // рисовать параболу с уравнением y = 10x^2 - 0x + 0
 
-	if (width <= height)
-	{
-		glOrtho(-10.0, 10.0, -10.0 * height / width, 10.0 * height / width, -1, 1);
-	}
-	else
-	{
-		glOrtho(-10.0 * width / height, 10.0 * width / height, -10.0, 10.0, -1, 1);
-	}
-
-	glMatrixMode(GL_MODELVIEW);
+	glFlush();
 }
 
 int main()
 {
-	if (!glfwInit())
-		return -1;
-
-	GLFWwindow* window = glfwCreateWindow(WIDTH, HEIGHT, "Parabola", NULL, NULL);
-
-	if (!window)
-	{
-		glfwTerminate();
-		return -1;
-	}
-
+	glfwInit();
+	GLFWwindow* window = glfwCreateWindow(800, 600, "OpenGL Parabola", NULL, NULL);
 	glfwMakeContextCurrent(window);
-	 resize(WIDTH, HEIGHT);
+
+	glMatrixMode(GL_PROJECTION);
+	glLoadIdentity();
+	glOrtho(-10.0, 10.0, -10.0, 10.0, -1.0, 1.0);
 
 	while (!glfwWindowShouldClose(window))
 	{
+		glClearColor(0.0, 0.0, 0.0, 1.0);
 		glClear(GL_COLOR_BUFFER_BIT);
 
-		drawX(0.0f, 0.0f);
-		drawParabola(1.0f, 2.0f, 2.0f);
+		display();
 
 		glfwSwapBuffers(window);
 		glfwPollEvents();
 	}
 
+	glfwDestroyWindow(window);
 	glfwTerminate();
 
 	return 0;
